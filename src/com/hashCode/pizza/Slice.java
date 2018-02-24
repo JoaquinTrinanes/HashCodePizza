477884f7b53d5b7130da72122293ae7ca3e10d9e
JoaquinTrinanes <quin.trinanes@gmail.com>
2018-02-24 15:53:32 +0100

    Ahora tenemos una clase Slice

---
+++ src/com/hashCode/pizza/Slice.java
@@ -0,0 +1,18 @@
+package com.hashCode.pizza;
+
+
+import java.awt.Rectangle;
+
+public class Slice {
+    private Rectangle forma;
+
+    public Slice() {
+        //x,y son el punto superior izquierdo
+        /*
+        Creo que puede ser interesante usar esta clase porque tiene metodos como contains y demas para ver si se
+        intercalan los trozos que tenemos, pero a ver
+         */
+        forma = new Rectangle(0, 0, 10,10);
+
+    }
+}
